454b6ee9eb4bc714594cb45c707c45ed274a5d2e
unknown <lahuynhthanhtam@gmail.com>
2022-03-16 21:02:36 +0700

    first commit

---
+++ Product/product.js
@@ -0,0 +1,10 @@
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+// const minus = $('.app-product-number__item-minus')
+// const plus = $('.app-product-number__item-plus')
+// const number = $('.app-product-number__item-text')
+
+var minus = document.getElementById('gl')
+
+minus.innerHTML = 'lý gia hân'
+console.log(minus)
